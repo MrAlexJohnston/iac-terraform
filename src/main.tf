6c9d1b346303894ed14a4804c0490a1bcac54ead
Alex Johnston <alex@Alexs-MacBook-Pro.local>
2023-01-30 19:34:34 +0000

    Adding private and public subnets with different AZ's

--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,31 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
  cidr_block = "10.192.0.0/16"
  
  tags = {
    Name = "Test VPC"
+ }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = data.aws_availability_zones.available.names[count.index]
+ 
+ tags = {
+   Name = "Public Subnet (AZ${count.index + 1})"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ availability_zone = data.aws_availability_zones.available.names[count.index]
+ 
+ tags = {
+   Name = "Private Subnet (AZ${count.index + 1})"
  }
 }
