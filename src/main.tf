5f6064dceb56abede8a96a391170265d42e68235
MrAlexJohnston <alex-johnston@live.co.uk>
2023-01-30 19:37:12 +0000

    Adding internet gateway

--- src/main.tf
+++ src/main.tf
@@ -29,3 +29,11 @@ resource "aws_subnet" "private_subnets" {
    Name = "Private Subnet (AZ${count.index + 1})"
  }
 }
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
