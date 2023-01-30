77674a324a41a51c0a01155d4c354e89152ebac3
MrAlexJohnston <alex-johnston@live.co.uk>
2023-01-30 19:42:02 +0000

    Adding public route table

--- src/main.tf
+++ src/main.tf
@@ -37,3 +37,16 @@ resource "aws_internet_gateway" "gw" {
    Name = "Project VPC IG"
  }
 }
+
+resource "aws_route_table" "public_route_table" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "Public Route Table"
+ }
+}
